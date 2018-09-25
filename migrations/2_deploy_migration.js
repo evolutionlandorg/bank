7359e6c715e75def7be3df27d93bb25f380e0aca
Alex Chien <alex.chien@koocaa.com>
2018-09-25 16:31:39 +0800

    Testing logic should be removed from migration

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -14,7 +14,12 @@ module.exports = function(deployer, network, accounts) {
 
 function deployOnLocal(deployer, network, accounts) {
     console.log(network);
-    
+
+    let conf = {
+      bank_unit_interest: 1015 * 10**15,
+      bank_penalty_multiplier: 3
+    }
+
     deployer.deploy([
         SettingsRegistry,
         DeployAndTest
@@ -40,36 +45,14 @@ function deployOnLocal(deployer, network, accounts) {
         // default settings
         // interest is about 1.015 KTON
         let bank_unit_interest = await bank.UINT_BANK_UNIT_INTEREST.call();
-        await registry.setUintProperty(bank_unit_interest, 1015 * 10**15);
+        await registry.setUintProperty(bank_unit_interest, conf.bank_unit_interest);
 
         let bank_penalty_multiplier = await bank.UINT_BANK_PENALTY_MULTIPLIER.call();
-        await registry.setUintProperty(bank_penalty_multiplier, 3);
+        await registry.setUintProperty(bank_penalty_multiplier, conf.bank_penalty_multiplier);
 
         await StandardERC223.at(kton).setOwner(GringottsBank.address);
 
-        let interest = await bank.computeInterest.call(10000, 12, 1015 * 10**15);
-        console.log("Current interest is: ... " + interest);
-    }).then(async () => {
-        let instance = await DeployAndTest.deployed();
-
-        let ring  =  await instance.testRING.call();
-        let kton  =  await instance.testKTON.call();
-        let kton_owner = await StandardERC223.at(kton).owner.call();
-        console.log(kton_owner);
-
-        console.log("Loging: bank..." + GringottsBank.address);
-
-        await StandardERC223.at(ring).mint(accounts[0], 10000 * 10**18);
-
-        // Need to run `web3.eth.defaultAccount = web3.eth.accounts[0]` before `migrate`
-        // give enough of gas for executing tx
-        await StandardERC223.at(ring).contract.transfer['address,uint256,bytes'](GringottsBank.address, 100 * 10**18, '0x0c', {gas: 300000});
-
-        // query amount of kton in console.
-        // DeployAndTest.deployed().then(function(instance){return instance.testKTON.call();}).then(function(addr){return StandardERC223.at(addr).then(function(instance){return instance.balanceOf.call(web3.eth.accounts[0])}).then(function(x){return x.toNumber()})})
+        let interest = await bank.computeInterest.call(10000, 12, conf.bank_unit_interest);
+        console.log("Current annual interest for 10,000 RING is: ... " + interest + " KTON");
     });
-
-
-    // in `truffle develop` console
-    // refer https://ethereum.stackexchange.com/questions/12957/truffle-invalid-address
 }
