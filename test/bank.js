d91bad38dc6af6b5b01793dc68359da5329dfd1c
WoeOm <1015996366@qq.com>
2019-11-06 18:15:09 +0800

    add test burndrop

--- test/bank.js
+++ test/bank.js
@@ -8,30 +8,34 @@ const GringottsBankProxy = artifacts.require("./OwnedUpgradeabilityProxy.sol")
 const gasPrice = 22000000000;
 const COIN = 10 ** 18;
 
-const increaseTime = function(duration) {
-    const id = Date.now()
-  
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.sendAsync({
-        jsonrpc: '2.0',
-        method: 'evm_increaseTime',
-        params: [duration],
-        id: id,
-      }, err1 => {
-        if (err1) return reject(err1)
-  
-        web3.currentProvider.sendAsync({
-          jsonrpc: '2.0',
-          method: 'evm_mine',
-          id: id+1,
-        }, (err2, res) => {
-          return err2 ? reject(err2) : resolve(res)
-        })
-      })
-    })
-  }
+function toWei(ether) {
+    return `${ether}000000000000000000`
+}
 
-contract('Gringotts Bank Test', async(accounts) => {
+const increaseTime = function (duration) {
+    const id = Date.now()
+
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.send({
+            jsonrpc: '2.0',
+            method: 'evm_increaseTime',
+            params: [duration],
+            id: id,
+        }, err1 => {
+            if (err1) return reject(err1)
+
+            web3.currentProvider.send({
+                jsonrpc: '2.0',
+                method: 'evm_mine',
+                id: id + 1,
+            }, (err2, res) => {
+                return err2 ? reject(err2) : resolve(res)
+            })
+        })
+    })
+}
+
+contract('Gringotts Bank Test', async (accounts) => {
     let deployer = accounts[0];
     let investor = accounts[1];
     let investor2 = accounts[2];
@@ -40,15 +44,15 @@ contract('Gringotts Bank Test', async(accounts) => {
     let ring;
     let kton;
 
-    before('deploy and configure', async() => {
+    before('deploy and configure', async () => {
         // get contract from deployed version
-        bank     = await GringottsBank.at(GringottsBankProxy.address); //await GringottsBank.deployed();
+        bank = await GringottsBank.at(GringottsBankProxy.address); //await GringottsBank.deployed();
         registry = await SettingsRegistry.deployed();
 
         let ring_settings = await bank.CONTRACT_RING_ERC20_TOKEN.call();
         let kton_settings = await bank.CONTRACT_KTON_ERC20_TOKEN.call();
-        ring = StandardERC223.at(await registry.addressOf.call(ring_settings))
-        kton = StandardERC223.at(await registry.addressOf.call(kton_settings))
+        ring = await StandardERC223.at(await registry.addressOf.call(ring_settings))
+        kton = await StandardERC223.at(await registry.addressOf.call(kton_settings))
 
         console.log('Bank address: ', bank.address);
         console.log('registry address: ', registry.address);
@@ -56,10 +60,10 @@ contract('Gringotts Bank Test', async(accounts) => {
         console.log('KTON address: ', kton.address);
 
         // give some ring to investor
-        await ring.mint(investor, 100000 * COIN, { from:deployer } );
+        await ring.mint(investor, toWei(100000), {from: deployer});
     })
 
-    it('bank setting should be same as registry initialization', async() => {
+    it('bank setting should be same as registry initialization', async () => {
         let bank_unit_interest = await registry.uintOf(await bank.UINT_BANK_UNIT_INTEREST.call());
         let bank_penalty_multiplier = await registry.uintOf(await bank.UINT_BANK_PENALTY_MULTIPLIER.call());
 
@@ -67,15 +71,21 @@ contract('Gringotts Bank Test', async(accounts) => {
         assert.equal(bank_penalty_multiplier, 3)
     })
 
-    it('should return correct amount of KTON', async() => {
+    it('should return correct amount of KTON', async () => {
         // deposit 100 RING for 1 year
-        await ring.contract.transfer['address,uint256,bytes']( bank.address, 10000 * COIN, '0x' + abi.rawEncode(['uint256'], [12]).toString('hex'), { from: investor, gas: 300000 });
+        await ring.methods['transfer(address,uint256,bytes)'](bank.address, toWei(10000), '0x' + abi.rawEncode(['uint256'], [12]).toString('hex'), {
+            from: investor,
+            gas: 300000
+        });
+
         let ktonAmount1 = await kton.balanceOf(investor);
-        
-        assert.equal(ktonAmount1.toNumber(), 1 * COIN);
+        assert.equal(ktonAmount1.toString(), toWei(1));
 
 
-        await ring.contract.transfer['address,uint256,bytes']( bank.address, 30000 * COIN, '0x' + abi.rawEncode(['uint256'], [12]).toString('hex'), { from: investor, gas: 300000 });
+        await ring.methods['transfer(address,uint256,bytes)'](bank.address, toWei(30000), '0x' + abi.rawEncode(['uint256'], [12]).toString('hex'), {
+            from: investor,
+            gas: 300000
+        });
 
         let ktonAmount = await kton.balanceOf(investor);
 
@@ -87,27 +97,30 @@ contract('Gringotts Bank Test', async(accounts) => {
 
         // console.log(deposit);
 
-        assert.equal(userTotal.toNumber(), 40000 * COIN);
+        assert.equal(userTotal.toString(), toWei(40000));
 
-        assert.equal(depositId.toNumber(), 1);
+        assert.equal(depositId.toString(), '1');
 
         assert.equal(deposit[0], investor);
-        assert.equal(deposit[1].toNumber(), 30000 * COIN);
-        assert.equal(deposit[2].toNumber(), 12);
-        assert.equal(deposit[4].toNumber(), 1000);
+        assert.equal(deposit[1].toString(), toWei(30000));
+        assert.equal(deposit[2].toString(), '12');
+        assert.equal(deposit[4].toString(), '1000');
         assert.equal(deposit[5], false);
 
-        assert.equal(ktonAmount.toNumber(), 4 * COIN);
+        assert.equal(ktonAmount.toString(), toWei(4));
     })
 
-    it('should deduct correct amount of penalty', async() => {
+    it('should deduct correct amount of penalty', async () => {
         let penalty = await bank.computePenalty.call(0);
 
-        console.log("Penalty is ... " + penalty.toNumber());
-        assert.equal(penalty.toNumber(), 3 * COIN);
-        
+        console.log(`Penalty is ... ${penalty}`);
+        assert.equal(penalty.toString(), toWei(3));
 
-        await kton.contract.transfer['address,uint256,bytes']( bank.address, 3 * COIN, '0x' + abi.rawEncode(['uint256'], [0]).toString('hex'), { from: investor, gas: 300000 });
+
+        await kton.methods['transfer(address,uint256,bytes)'](bank.address, toWei(3), '0x' + abi.rawEncode(['uint256'], [0]).toString('hex'), {
+            from: investor,
+            gas: 300000
+        });
 
         let userTotal2 = await bank.userTotalDeposit.call(investor);
 
@@ -116,16 +129,15 @@ contract('Gringotts Bank Test', async(accounts) => {
         let deposit2 = await bank.getDeposit.call(depositId2);
         let ktonAmount2 = await kton.balanceOf(investor);
 
-        assert.equal(userTotal2.toNumber(), 30000 * COIN);
+        assert.equal(userTotal2.toString(), toWei(30000));
 
-        assert.equal(depositId2.toNumber(), 0);
+        assert.equal(depositId2.toString(), '0');
         assert.equal(deposit2[5], true);
-        
-        assert.equal(ktonAmount2.toNumber(), 1 * COIN);
 
+        assert.equal(ktonAmount2.toString(), toWei(1));
     })
 
-    it('should transfer owner of deposit successful', async() => {
+    it('should transfer owner of deposit successful', async () => {
         await bank.transferDeposit(investor2, 1, {from: investor, gas: 300000});
         let deposit = await bank.getDeposit.call(1);
 
@@ -138,15 +150,15 @@ contract('Gringotts Bank Test', async(accounts) => {
     })
 
     // need help with timecop
-    it('should be able to redeem back all ring when due', async() => {
+    it('should be able to redeem back all ring when due', async () => {
         // time flies, 13 months passed
         increaseTime(60 * 60 * 24 * 30 * 13);
 
         let ringAmount1 = await ring.balanceOf(investor);
 
-        console.log("RING amount 1 ... " + ringAmount1.toNumber());
+        console.log(`RING amount 1 ... ${ringAmount1}`);
 
-        await bank.claimDeposit( 1, { from: investor, gas: 300000 } );
+        await bank.claimDeposit(1, {from: investor, gas: 300000});
 
         let ktonAmount = await kton.balanceOf(investor);
 
@@ -159,16 +171,42 @@ contract('Gringotts Bank Test', async(accounts) => {
 
         let ringAmount2 = await ring.balanceOf(investor);
 
-        console.log("RING amount 2 ... " + ringAmount2.toNumber());
+        console.log(`RING amount 2 ... ${ringAmount2}`);
 
-        assert.equal(ringAmount2.minus(ringAmount1).toNumber(), 30000 * COIN);
+        assert.equal(ringAmount2.sub(ringAmount1).toString(), toWei(30000));
     })
 
-    it('test bytesToUint256', async() => {
+    it('test bytesToUint256', async () => {
         console.log('0x' + abi.rawEncode(['uint256'], [12]).toString('hex'));
         let x = await bank.bytesToUint256.call('0x' + abi.rawEncode(['uint256'], [12]).toString('hex'));
 
         assert.equal(x, 12);
     })
 
+    it('test burndrop', async () => {
+        let userTotal0 = await bank.userTotalDeposit.call(investor);
+
+        assert.equal(userTotal0.toString(), '0');
+
+        await ring.methods['transfer(address,uint256,bytes)'](bank.address, toWei(10000), '0x' + abi.rawEncode(['uint256'], [12]).toString('hex'), {
+            from: investor,
+            gas: 300000
+        });
+        let depositId = await bank.userDeposits.call(investor, 2);
+        let deposit = await bank.getDeposit.call(depositId);
+
+        let userTotal = await bank.userTotalDeposit.call(investor);
+
+        assert.equal(userTotal.toString(), toWei(10000));
+
+        console.log('burndrop params: ', deposit, depositId, investor, deposit[1], deposit[2], deposit[3], deposit[4], '0x');
+
+        await bank.burndrop(depositId, investor, deposit[2], deposit[3], deposit[4], deposit[1], '0x', {
+            from: investor,
+            gas: 300000
+        })
+
+        let userTotal1 = await bank.userTotalDeposit.call(investor);
+        assert.equal(userTotal1.toString(), '0');
+    })
 })
